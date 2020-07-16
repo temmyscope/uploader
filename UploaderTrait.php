9b8cdbffea35c83aa2fc341403db51bafd503aff
temmyscope <esdentp@gmail.com>
2020-07-16 08:50:19 -0700

    Code Improvement and dimension functionality

--- UploaderTrait.php
+++ UploaderTrait.php
@@ -1,6 +1,6 @@
 <?php
 namespace Seven\File;
-
+use Seven\Vars\Strings;
 
 /**
  * Should be used in a model class that defines all of the initialised variables 
@@ -26,51 +26,45 @@ trait UploaderTrait{
 	* @example 5024768; This is the default value, which is approximately 5mb
 	*/
 
-	protected $resize = false;
-	protected $dim = [];
-
-	public function upload($file): array
+	public function upload($file, $dim = []): array
 	{
 		if($file['size'] > $this->_limit){
 			return ['status' => false, 'value' => "The file limit of ".($this->_limit/1048576)." mb has been exceeded"];
 		}
-		[$target, $type] = $this->getNewName($file['name']);
-		if( $this->move( $file["tmp_name"], $target ) ){
-        	if( $this->allowed( $type, $this->getMime($target) ) ){
-		    	return ['status' => false, 'value' => "This file type or mime is not allowed"];
-			}else{
-				if ( $this->resize ) {
-					$this->resizer( $target, $type, $this->dim[0], $this->dim[1] );
+		[$target, $type] = $this->getNewName( $file['name'] );
+		if ( empty($file["tmp_name"]) ) {
+			return ['status' => false, 'value' => "This file has no temporary name in its data, use another file."];
+		}
+    	if ($this->move( $file["tmp_name"], $target )){
+    		if( $this->allowed( $type, $this->getMime($target) ) ){
+    			if ( !empty($dim) ) {
+					$this->resizer( $target, $type, $dim[0], $dim[1] );
 				}
-            	return ['status' => true, 'value' => $target, 'type' => $type, 'size' => $file['size'] ];
-			}
-        }
-	}
-
-	//only applicable for image uploads
-	public function resize($width, $height): self
-	{
-		$this->resize = true;
-		$this->dim = [ $width, $height ];
-		return $this;
+	        	return ['status' => true, 'value' => $target, 'type' => $type, 'size' => $file['size'] ];
+    		}else{
+    			rmdir($target);
+    		}
+    		return ['status' => false, 'value' => "This file type or mime is not allowed"];
+		}
+		return ['status' => false, 'value' => "An unknown error occurred."];
 	}
 
 	private function resizer($file, $ext, $w, $h)
 	{
 		list($width, $height) = getimagesize($file);
-	   	if($ext == "jpg" || $ext == "jpeg"){
+	   	if($ext === "jpg" || $ext === "jpeg"){
 		   	$src = imagecreatefromjpeg($file);
 		   	$dst = imagecreatetruecolor($w, $h);
 		   	imagecopyresampled($dst, $src, 0, 0, 0, 0, $w, $h, $width, $height);
 		   	// again for jpg
 		   	imagejpeg($dst, $file);
-	   	}elseif($ext == "png"){
+	   	}elseif($ext === "png"){
 	   		$src = imagecreatefrompng($file);
 	   		$dst = imagecreatetruecolor($w, $h);
 		   	imagecopyresampled($dst, $src, 0, 0, 0, 0, $w, $h, $width, $height);
 		   	// again for png
 		   	imagepng($dst, $file);
-	   	}elseif($ext == "gif"){
+	   	}elseif($ext === "gif"){
 	   		$src = imagecreatefromgif($file);
 	   		$dst = imagecreatetruecolor($w, $h);
 		   	imagecopyresampled($dst, $src, 0, 0, 0, 0, $w, $h, $width, $height);
@@ -81,7 +75,7 @@ trait UploaderTrait{
 
 	private function move($source, $dest): bool
 	{
-		return move_uploaded_file($source, $target);
+		return move_uploaded_file($source, $dest);
 	}
 
 	private function getMime($filename)
